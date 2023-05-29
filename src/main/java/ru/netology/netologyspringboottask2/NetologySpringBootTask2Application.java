e8b5888c477dcb2eaf4eb9cfc8c564a168e77e5d
Michail Evseev <mixaevseev1989@gmail>
2023-05-29 21:42:06 +0300

    Initial commit

---
+++ src/main/java/ru/netology/netologyspringboottask2/NetologySpringBootTask2Application.java
@@ -0,0 +1,13 @@
+package ru.netology.netologyspringboottask2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetologySpringBootTask2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetologySpringBootTask2Application.class, args);
+    }
+
+}
---
+++ src/main/java/ru/netology/netologyspringboottask2/model/Authorities.java
@@ -0,0 +1,5 @@
+package ru.netology.netologyspringboottask2.model;
+
+public enum Authorities {
+    READ, WRITE, DELETE
+}
